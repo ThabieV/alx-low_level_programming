9b0c2579abcf3d40e40510c83a5d758837b8ebff
ThabieV <tkganyego@gmail.com>
2023-02-17 19:17:50 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@ int main(void)
 
 {
 
-	const char* str = ("Programming is like building a multilingual puzzle\n)";
+	const char* str = ("\"Programming is like building a multilingual puzzle"\n);
 	
 	puts(str);
 	
